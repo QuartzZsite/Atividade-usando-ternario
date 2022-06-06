f3412ffe342786c5a6448d73ba909a30b799f680
Carlos Augusto Ferreira Freire <carlos.freire@programmers.com.br>
2022-06-06 10:39:25 -0300

    primeiro commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿int nota = int.Parse(Console.ReadLine());
+
+/*
+if (nota < 5)
+{
+    Console.WriteLine("Reprovado");
+}
+else if (nota > 5 && nota < 8)
+{
+    Console.WriteLine("Recuperação");
+}
+else
+{
+    Console.WriteLine("Aprovado");
+}
+*/
+
+var resultado = nota < 8 ? "Reprovado" : "Aprovado";
+Console.WriteLine(resultado);
